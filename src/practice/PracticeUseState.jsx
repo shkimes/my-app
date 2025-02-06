b3f822a2c628320a48b838207e7fde3e077db5e3
shkimes <shkimes09@gmail.com>
2025-02-06 12:21:07 +0900

    react-start

---
+++ src/practice/PracticeUseState.jsx
@@ -0,0 +1,27 @@
+
+
+function PracticeUseState(){
+
+}
+
+function UseStateOne(){
+    //사용자가 입력한 텍스트를 화면에 표시하는 컴넌트 생성
+
+    return(
+        <div>
+            <input type="text" placeholder="텍스트를 입력하세요"
+            value={   }
+                   onChange={(e)=>(e.target.value)}
+            />
+            <p>입력한 값 : {    }</p>
+        </div>
+    )
+
+}
+
+function UseStateTwo(){
+    //랜덤으로 숫자 생성하기
+
+    //랜덤 숫자 기능
+    //Math.floor(Math.random()
+}
